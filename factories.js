de9a4f73e1e7a747cd208818ab5379fb57d41c8c
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2022-06-15 09:51:54 +0200

    sauvegarde avant changement des onglets

--- factories.js
+++ factories.js
@@ -1,14 +1,8 @@
 export function recipesFactory(data) {
+
     const { name, time, ingredients, description } = data
 
-    // let ingredientsName = ""
-
-    // function namingIngredients() {
-    //     for (let i = 0; i < ingredients.length; i++) {
-    //        
-    //         ingredientsList = ingredients[i].textContent
-    //     }
-    // }
+    // Faire un push de l'ingrédient trouvé
 
     function getUserRecipe() {
         const article = document.createElement("article")
@@ -20,17 +14,39 @@ export function recipesFactory(data) {
         const recipeName = document.createElement("h3")
         recipeName.setAttribute("class", "recipe-name")
         const timeIcon = document.createElement("i")
-        timeIcon.setAttribute("class", "fa-regular fa-clock")
+        timeIcon.setAttribute("class", "far fa-clock")
         timeIcon.classList.add("time-icon")
         const timeSpan = document.createElement("span")
         timeSpan.setAttribute("class", "time-span")
+
+        let ingredientsString = ""
+
+        for (let i = 0; i < ingredients.length; i++) {
+
+            // if (ingredients[j].unit === undefined) {
+            //     ingredients[j].unit = ""
+            // }
+
+            // if (ingredients[j].quantity === undefined) {
+            //     ingredients[j].quantity = ""
+            // }
+
+            let row = `<li>${ingredients[i].ingredient} ${ingredients[i].quantity || ""} ${ingredients[i].unit || ""}</li>`
+
+            ingredientsString += row
+
+        }
+
+
+
+
         const ingredientsList = document.createElement("p")
         ingredientsList.setAttribute("class", "ingredients-list")
         const recipeDescription = document.createElement("p")
         recipeDescription.setAttribute("class", "recipe-description")
         recipeName.textContent = name
         timeSpan.textContent = `${time} min`
-        ingredientsList.textContent = ingredients
+        ingredientsList.innerHTML = ingredientsString
         recipeDescription.textContent = description
 
         article.appendChild(pictureDiv)
@@ -48,16 +64,58 @@ export function recipesFactory(data) {
 
 }
 
-// function ingredientsFactory(data) {
-//     const {ingredients} = data
+export function ingredientsListFactory(ingredientsArray) {
 
-//     function getUserIngredients() {
+    function getUserIngredientsList() {
+        const a = document.createElement("a")
+        a.setAttribute("class", "menu-link")
+        const span = document.createElement("a")
+        span.setAttribute("class", "menu-name")
 
-//     const article = document.createElement("article")
-//     article.setAttribute("class", "ingredients-test-article")
-//     const p = document.createElement("p")
+        for (let i = 0; i < ingredientsArray.length; i++) {
+            span.textContent = `${ingredientsArray} `
+        }
+
+        a.appendChild(span)
+        return (a)
+    }
+    return { getUserIngredientsList }
+}
+
+export function devicesListFactory(devicesArray) {
+
+    function getUserDevicesList() {
+        const a = document.createElement("a")
+        a.setAttribute("class", "menu-link")
+        const span = document.createElement("a")
+        span.setAttribute("class", "menu-name")
+        for (let i = 0; i < devicesArray.length; i++) {
+            span.textContent = devicesArray
+        }
+
+        a.appendChild(span)
+        return (a)
+    }
+
+    return { getUserDevicesList }
+}
+
+export function utensilsListFactory(utensilsArray) {
+
+    function getUserUtensilsList() {
+        const a = document.createElement("a")
+        a.setAttribute("class", "menu-link")
+        const span = document.createElement("a")
+        span.setAttribute("class", "menu-name")
+
+        for (let i = 0; i < utensilsArray.length; i++) {
+            span.textContent = utensilsArray
+        }
+
+        a.appendChild(span)
+        return (a)
+    }
 
 
-//     return (article)
-// }
-// }
+    return { getUserUtensilsList }
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 import { recipes } from "./recipes.js"
-import { recipesFactory } from "./factories.js"
+import { recipesFactory, ingredientsListFactory, devicesListFactory, utensilsListFactory } from "./factories.js"
 
 const ingredientsInput = document.getElementById("ingredients-check")
 const devicesInput = document.getElementById("devices-check")
@@ -9,21 +9,132 @@ const utensilsInput = document.getElementById("utensils-check")
 const ingredientsMenu = document.querySelector(".menu__ingredients")
 const devicesMenu = document.querySelector(".menu__devices")
 const utensilsMenu = document.querySelector(".menu__utensils")
-// const ingredientsDropdown = document.querySelector(".ingredients-nav")
-// const devicesDropdown = document.querySelector(".devices-nav")
-// const utensilsDropdown = document.querySelector(".utensils-nav")
 
 const recipesArray = recipes
+let ingredientsArray = []
+let devicesArray = []
+let utensilsArray = []
+let namesArray = []
+let descriptionsArray = []
 
 function findingIngredients(recipes) {
+
+
+
     for (let i = 0; i < recipes.length; i++) {
 
-        return recipes[i].ingredients
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+            ingredientsArray.push(recipes[i].ingredients[j].ingredient)
+        }
     }
+
+    console.log(ingredientsArray)
+
+    ingredientsArray = [...new Set(ingredientsArray)]
+
+    return ingredientsArray;
 }
 
-const findingIngredients2 = findingIngredients(recipes);
-console.log(findingIngredients2)
+function findingDevices(recipes) {
+
+
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        devicesArray.push(recipes[i].appliance)
+    }
+
+    console.log(devicesArray)
+
+    devicesArray = [...new Set(devicesArray)]
+
+    return devicesArray;
+}
+
+function findingUtensils(recipes) {
+
+
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        for (let j = 0; j < recipes[i].ustensils.length; j++) {
+            utensilsArray.push(recipes[i].ustensils[j])
+        }
+    }
+
+    console.log(utensilsArray)
+
+    utensilsArray = [...new Set(utensilsArray)]
+
+    return utensilsArray;
+}
+
+function findingNames(recipes) {
+
+
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        namesArray.push(recipes[i].name)
+    }
+
+    console.log(namesArray)
+
+    return namesArray;
+}
+
+function findingDescriptions(recipes) {
+
+
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        descriptionsArray.push(recipes[i].description)
+    }
+
+    console.log(descriptionsArray)
+
+    return descriptionsArray;
+}
+
+
+function search() {
+
+    const searchInput = document.querySelector(".search-bar")
+
+
+    function launchResearch() {
+
+        // rajouter le filter après
+
+        let searchingString = `${ingredientsArray} ${namesArray} ${descriptionsArray}`
+        console.log(searchingString)
+
+        let index = searchingString.indexOf(searchInput.value)
+
+        if (index !== -1 && searchInput.value.length >= 3) {
+            alert("ça marche !")
+        } else {
+            console.log("ça ne marche pas !")
+        }
+
+        console.log(index)
+
+
+        // if (photosId === photographerId2) {
+        //     return elem.photographerId === photosId;
+        // }
+    }
+
+
+    searchInput.addEventListener("input", () => {
+        // if (searchData.length >= 3) {
+        //     launchResearch()
+        // }
+        launchResearch()
+    })
+}
+
 
 const displayMenu = (menu1, menu2, menu3) => {
     menu1.checked = true
@@ -47,17 +158,17 @@ function closeMenu(menu) {
 }
 
 function shiftMenu(input, sidemenu1, sidemenu2) {
-    if (input == ingredientsInput) {
+    if (input === ingredientsInput) {
         ingredientsMenu.style.transform = "translateX(" + 0 + "px)"
         sidemenu1.style.transform = "translateX(" + 0 + "px)"
         sidemenu2.style.transform = "translateX(" + 0 + "px)"
     }
-    else if (input == devicesInput) {
+    else if (input === devicesInput) {
         devicesMenu.style.transform = "translateX(" + -190 + "px)"
         sidemenu1.style.transform = "translateX(" + 687 + "px)"
         sidemenu2.style.transform = "translateX(" + 0 + "%)"
     }
-    else if (input == utensilsInput) {
+    else if (input === utensilsInput) {
         utensilsMenu.style.transform = "translateX(" + -380 + "px)"
         sidemenu1.style.transform = "translateX(" + 687 + "px)"
         sidemenu2.style.transform = "translateX(" + 687 + "px)"
@@ -67,6 +178,41 @@ function shiftMenu(input, sidemenu1, sidemenu2) {
 
 function growMenu(menu) {
     menu.style.width = 667 + "px"
+}
+
+function displayIngredientsList(ingredientsArray) {
+    const ingredientsList = document.getElementById("ingredients-list")
+
+    ingredientsArray.forEach((ingredient) => {
+        const ingredientsListModel = ingredientsListFactory(ingredient)
+        const userIngredientsList = ingredientsListModel.getUserIngredientsList()
+        ingredientsList.appendChild(userIngredientsList)
+    })
+
+}
+
+function displayDevicesList(devicesArray) {
+
+
+    const devicesList = document.getElementById("devices-list")
+
+    devicesArray.forEach((device) => {
+        const devicesListModel = devicesListFactory(device)
+        const userDevicesList = devicesListModel.getUserDevicesList()
+        devicesList.appendChild(userDevicesList)
+    })
+
+}
+
+function displayUtensilsList(utensilsArray) {
+    const utensilsList = document.getElementById("utensils-list")
+
+    utensilsArray.forEach((utensil) => {
+        const utensilsListModel = utensilsListFactory(utensil)
+        const userUtensilsList = utensilsListModel.getUserUtensilsList()
+        utensilsList.appendChild(userUtensilsList)
+    })
+
 }
 
 function tabListeners() {
@@ -95,6 +241,21 @@ function tabListeners() {
 
 function displayRecipes(recipes) {
 
+    recipes.sort((a, b) => {
+        a = a.name;
+        b = b.name;
+
+        if (a < b) {
+            return -1;
+        }
+        if (a > b) {
+            return 1;
+        }
+        if (a === b) {
+            return a.name - b.name;
+        }
+    })
+
     const recipesSection = document.querySelector(".recipes-section");
 
     recipes.forEach((recipe) => {
@@ -105,14 +266,20 @@ function displayRecipes(recipes) {
 
 }
 
-// function displayIngredients(recipes.ingredients)
-
-
 function init() {
 
     tabListeners()
+    console.log(recipes)
+    findingIngredients(recipes)
+    findingDevices(recipes)
+    findingUtensils(recipes)
+    findingNames(recipes)
+    findingDescriptions(recipes)
+    search()
+    displayIngredientsList(ingredientsArray)
+    displayDevicesList(devicesArray)
+    displayUtensilsList(utensilsArray)
     displayRecipes(recipesArray)
-    console.log(recipesArray)
 
 }
 
